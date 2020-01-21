46c05a0563eaf5cae6f138113d96de2170dec47c
Rafael Barros <rafaelbarros.df@gmail.com>
2020-01-21 06:49:22 -0300

    :construction: Usando soap no servidor aplicacao

---
+++ 06-usando-soap-no-servidor-aplicacao/estoquews/src/br/com/caelum/estoque/ws/EstoqueWs.java
@@ -0,0 +1,60 @@
+package br.com.caelum.estoque.ws;
+
+import br.com.caelum.estoque.modelo.exception.AutorizacaoException;
+import br.com.caelum.estoque.modelo.item.*;
+import br.com.caelum.estoque.modelo.usuario.TokenDao;
+import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+import static javax.jws.soap.SOAPBinding.*;
+import java.util.List;
+
+@WebService
+@SOAPBinding(style= Style.DOCUMENT, use = Use.LITERAL, parameterStyle = ParameterStyle.WRAPPED)
+public class EstoqueWs {
+
+    private ItemDao dao = new ItemDao();
+
+    @WebMethod(operationName = "TodosOsItens")
+    @WebResult(name = "itens")
+    public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) {
+        System.out.println("Chamando getItens()");
+        List<Filtro> lista = filtros.getLista();
+        List<Item> itensResultado = dao.todosItens(lista);
+        return new ListaItens(itensResultado);
+    }
+
+    @WebMethod(action="CadastrarItem", operationName = "CadastrarItem")
+    @WebResult(name = "item")
+    public Item cadastrarItem(@WebParam(name = "tokenUsuario", header = true) TokenUsuario  token,
+                              @WebParam(name = "item") Item item) throws AutorizacaoException {
+
+        System.out.println("cadastrando item " + item + ", Token: " + token);
+
+        boolean valido = new TokenDao().ehValido(token);
+
+        if (!valido) {
+            throw new AutorizacaoException("Autorização falhou");
+        }
+
+        //novo
+        new ItemValidador(item).validate();
+
+        this.dao.cadastrar(item);
+
+        return item;
+    }
+
+    @WebMethod(operationName = "CadastrarItem2")
+    @WebResult(name = "item")
+    public Item cadastrarItem2(@WebParam(name = "tokenUsuario", header = true) TokenUsuario  token,
+                              @WebParam(name = "item") Item item) throws AutorizacaoException {
+        return item;
+    }
+
+}
