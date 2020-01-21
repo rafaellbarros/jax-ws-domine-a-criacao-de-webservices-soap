5eb518ff545203f063bf08994ea07e7d91fbe4df
Rafael Barros <rafaelbarros.df@gmail.com>
2020-01-21 07:14:49 -0300

    :construction: Criando um cliente java

---
+++ 07-criando-um-cliente-java/estoquews-cliente/src/br/com/caelum/estoque/cliente/TesteServicoWeb.java
@@ -0,0 +1,20 @@
+package br.com.caelum.estoque.cliente;
+
+public class TesteServicoWeb {
+
+	public static void main(String[] args) {
+	
+		EstoqueWS cliente = new EstoqueWS_Service().getEstoqueWSImplPort();
+		
+		Filtros filtros = new Filtros();
+		Filtro filtro = new Filtro();
+		filtro.setNome("Iphone");
+		filtro.setTipo(TipoItem.LIVRO);
+		filtros.getFiltro().add(filtro);
+		
+	
+		ListaItens itens = cliente.todosOsItens(filtros);
+		
+		System.out.println(itens);
+	}
+}
